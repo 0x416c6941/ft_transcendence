cfda039e535dee603bca1ccbc753d8892876261e
Asset Sagymbayev <69522068+0x416c6941@users.noreply.github.com>
2025-10-05 21:10:41 +0200

    frontend: src/{HomeView.ts,NotFoundView.ts} - make views look better

--- frontend/src/views/HomeView.ts
+++ frontend/src/views/HomeView.ts
@@ -9,8 +9,8 @@ export default class HomeView extends AbstractView {
 
 async getHtml(): Promise<string> {
   return `
-    <main class="p-6">
-      <h1 class="text-3xl mb-4">Welcome to ${APP_NAME}</h1>
+    <main class="h-screen flex justify-center items-center flex-col bg-neutral-200 dark:bg-neutral-900">
+      <h1 class="txt-light-dark-sans text-3xl mb-4">Welcome to ${APP_NAME}</h1>
       <a href="/login" data-link
          class="bg-sky-500 text-white px-4 py-2 rounded shadow">Login</a>
     </main>
