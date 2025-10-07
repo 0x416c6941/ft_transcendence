41e0b8b552e4e13027eb2354586d4af357546279
gittwenstein <maxian@post.cz>
2025-10-07 18:47:54 +0200

    Change Swagger docs route, add a few lines in the README

--- backend/src/swagger/config.ts
+++ backend/src/swagger/config.ts
@@ -38,7 +38,7 @@ export const swaggerConfig = {
 };
 
 export const swaggerUIConfig = {
-	routePrefix: '/api/documentation',
+	routePrefix: '/api/docs',
 	uiConfig: {
 		docExpansion: 'list' as const,
 		deepLinking: false
