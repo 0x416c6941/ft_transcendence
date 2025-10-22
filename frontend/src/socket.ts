2b6442392a82f2594e1f5774f0f3d6a314f40324
gittwenstein <maxian@post.cz>
2025-10-22 20:10:38 +0200

    Put the enabled autoConnect back in socket.ts

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -8,9 +8,7 @@ const SOCKET_PATH = '/api/socket.io/';
 /**
  * @var {any} io
  * Socket.IO connection to the server.
- * Configured with autoConnect: false - will be connected with auth token in main.ts
  */
 export const io: any = (window as any).io(window.location.origin, {
-	path: SOCKET_PATH,
-	autoConnect: false
+	path: SOCKET_PATH
 });
